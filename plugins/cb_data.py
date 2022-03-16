166abbdb78269d83f1b99b0f8622184f28f3dcc9
KOSHI Dev <86915159+arjun-sangu@users.noreply.github.com>
2022-03-16 20:20:45 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -19,7 +19,7 @@ async def rename(bot,update):
 	user_id = update.message.chat.id
 	date = update.message.date
 	await update.message.delete()
-	await update.message.reply_text("__Please enter the new filename...__",	
+	await update.message.reply_text("__Please enter the new filename and reply this message...__",	
 	reply_to_message_id=update.message.reply_to_message.message_id,  
 	reply_markup=ForceReply(True))
 	
